de36e90b1760bfd339bff46e62c312420bfc1220
Cristiano Faustino <cristiadu@gmail.com>
2024-01-08 10:56:53 -0800

    adding initial linkedin tests

---
+++ my-li-jobs/tests/linkedin.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://www.linkedin.com/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle("LinkedIn: Log In or Sign Up");
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://www.linkedin.com/');
+
+  // Click the get started link.
+  await page.getByRole('button', { name: 'Sign In' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByText('Please enter an email address or phone number', { exact: true })).toBeVisible();
+});
