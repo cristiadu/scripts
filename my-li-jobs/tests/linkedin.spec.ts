e02474fb324513fc595d91341a44ac53c0340b86
Cristiano Faustino <cristiadu@gmail.com>
2024-01-08 17:41:22 -0800

    final state for today since linkedin is annoying

--- my-li-jobs/tests/linkedin.spec.ts
+++ my-li-jobs/tests/linkedin.spec.ts
@@ -1,18 +1,38 @@
 import { test, expect } from '@playwright/test';
+import dotenv from 'dotenv';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://www.linkedin.com/');
+test.describe('Get new jobs from LinkedIn', () => {
+  dotenv.config();
+  const searchTerms = process.env.SEARCH_TERMS?.split(',') || ['software'];
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle("LinkedIn: Log In or Sign Up");
-});
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://www.linkedin.com/');
+  
+    await expect(page).toHaveTitle("LinkedIn: Log In or Sign Up");
+  
+    // Log in into LinkedIn
+    await page.getByLabel('Email or phone').fill(process.env.USERNAME || 'unset');
+    await page.getByLabel('Password').first().fill(process.env.PASSWORD || 'unset');
+    await page.getByRole('button', { name: 'Sign In' }).click();
+  
+    // Wait until "Jobs" link appear on header
+    await expect(page.getByTitle('Jobs', { exact: true })).toBeVisible();
+  });
 
-test('get login error when empty', async ({ page }) => {
-  await page.goto('https://www.linkedin.com/');
+  // For each searchTerm, check all jobs.
+  for(const searchTerm of searchTerms) {
+    test(`fetch jobs matching term: ${searchTerm}`, async ({ page }) => {
+      await page.getByTitle('Jobs', { exact: true }).click();
 
-  // Click the get started link.
-  await page.getByRole('button', { name: 'Sign In' }).click();
+       // Wait until "Search" placeholder changes to Job specific one.
+      const searchBoxElement = page.getByRole('combobox', {name: 'Search by title, skill, or company', exact: true });
+      await expect(searchBoxElement).toBeVisible();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByText('Please enter an email address or phone number', { exact: true })).toBeVisible();
-});
+      await searchBoxElement.click().then(() => searchBoxElement.fill(searchTerm));
+      await page.getByRole('button', {name: 'Search', exact: true}).click();
+      await expect(page.getByTitle(`${searchTerm} in Canada`)).toBeVisible();
+    });
+  }
+})
+
+
